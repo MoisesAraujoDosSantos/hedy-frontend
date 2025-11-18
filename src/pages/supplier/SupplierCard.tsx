a8e423500ed9b9f2143f7d4ddc8ec864cae802d2
Moises Araujo dos Santos <moisesaraujodossantos10@gmail.com>
2025-11-17 23:55:28 -0300

    fix: arrumando o posicionamento dos dados do fornecedor

--- src/pages/supplier/SupplierCard.tsx
+++ src/pages/supplier/SupplierCard.tsx
@@ -1,30 +1,32 @@
 import type { SupplierType } from "../../types/SupplierType"
 
-export const SupplierCard = ({information}: {information: SupplierType[]}) => {
+export const SupplierCard = ({ information }: { information: SupplierType[] }) => {
     return (
         <>
-        <header className="flex gap-32 text-red-600">
-            <div className="flex gap-56">
-                <span>Nome Fantasia</span>
-                <span>Razão Social</span>
-                <span>CNPJ</span>
-                <span>Telefone</span>
-                <span>Data de Cadastro</span>
-            </div>
-            <span>operações</span>
-            
-        </header>
-            {information && (information.map((supplier: SupplierType) => (
+            <div className="flex flex-col gap-4 bg-[#141A29] ml-10 border-2 w-[1520px] h-[872px]">
+                <header className="flex gap-4 bg-[#20273C] text-red-600">
+                    <div className="flex gap-40 w-[1400px]">
+                        <p className="ml-5">Nome Fantasia</p>
+                        <p>Razão Social</p>
+                        <p>CNPJ</p>
+                        <p>Telefone</p>
+                        <p>Data de Cadastro</p>
+                    </div>
+                    <span className="mr-14">operações</span>
 
-                <div key={supplier.id} className="flex gap-32 w-[1520px] text-amber-300">
-                    <p className="w-243">{supplier.legal_name}</p>
-                    <p className="w-full">{supplier.trade_name}</p>
-                    <p className="w-full">{supplier.cnpj}</p>
-                    <p className="w-full">{supplier.value}</p>
-                    <p className="w-full">{supplier.registration_date}</p>
-                    <div className="flex gap-2 w-fit"><p>editar</p> <p>excluir</p></div>
-                </div>
-            )))}
+                </header>
+                {information && (information.map((supplier: SupplierType) => (
+
+                    <div key={supplier.id} className="flex gap-4 ml-5 border-2 w-[96%] text-amber-300">
+                        <p className="w-full">{supplier.legal_name}</p>
+                        <p className="w-full">{supplier.trade_name}</p>
+                        <p className="w-full">{supplier.cnpj}</p>
+                        <p className="w-full">{supplier.value}</p>
+                        <p className="w-full">{supplier.registration_date}</p>
+                        <div className="flex gap-2 w-fit"><p>editar</p> <p>excluir</p></div>
+                    </div>
+                )))}
+            </div>
         </>
     )
 }
