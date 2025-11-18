32a52209ade75efb337597863ff25c5e9637f800
Moises Araujo dos Santos <moisesaraujodossantos10@gmail.com>
2025-11-18 16:15:07 -0300

    fix: arrumando a funcionalidade de listar do  componente de fornecedor

--- src/pages/supplier/SupplierCard.tsx
+++ src/pages/supplier/SupplierCard.tsx
@@ -3,29 +3,42 @@ import type { SupplierType } from "../../types/SupplierType"
 export const SupplierCard = ({ information }: { information: SupplierType[] }) => {
     return (
         <>
-            <div className="flex flex-col gap-4 bg-[#141A29] ml-10 border-2 w-[1520px] h-[872px]">
-                <header className="flex gap-4 bg-[#20273C] text-red-600">
-                    <div className="flex gap-40 w-[1400px]">
+
+            <div className="flex flex-col gap-4 bg-[#141A29] mt-10 ml-10 rounded-[10px] w-[1520px] h-[872px]">
+                <p className="mt-5 ml-5 font-semibold text-[#E0E2EB] text-2xl">Listagem </p>
+                <header className="items-center grid grid-cols-6 bg-[#20273C] w-full h-[50px] text-[#858CA3]">
                         <p className="ml-5">Nome Fantasia</p>
-                        <p>Razão Social</p>
-                        <p>CNPJ</p>
-                        <p>Telefone</p>
-                        <p>Data de Cadastro</p>
-                    </div>
-                    <span className="mr-14">operações</span>
+                        <p className="ml-5">Razão Social</p>
+                        <p className="ml-5">CNPJ</p>
+                        <p className="ml-5">Telefone</p>
+                        <p className="ml-5">Data de Cadastro</p>
+                        <span className="mr-14 ml-5">operações</span>
+                    
 
                 </header>
                 {information && (information.map((supplier: SupplierType) => (
 
-                    <div key={supplier.id} className="flex gap-4 ml-5 border-2 w-[96%] text-amber-300">
-                        <p className="w-full">{supplier.legal_name}</p>
-                        <p className="w-full">{supplier.trade_name}</p>
-                        <p className="w-full">{supplier.cnpj}</p>
-                        <p className="w-full">{supplier.value}</p>
-                        <p className="w-full">{supplier.registration_date}</p>
+                    <div key={supplier.id} className="grid grid-cols-6 ml-5 w-full text-amber-50">
+                        <p className="w-[230px] h-10">{supplier.legal_name}</p>
+                        <p className="w-[230px] h-10">{supplier.trade_name}</p>
+                        <p className="w-[230px] h-10">{supplier.cnpj}</p>
+                        <p className="w-[230px] h-10">{supplier.value}</p>
+                        <p className="w-[230px] h-10">{supplier.registration_date}</p>
                         <div className="flex gap-2 w-fit"><p>editar</p> <p>excluir</p></div>
+                        <span className="col-span-6 border-[#20273C] border-b w-[97%]"></span>
                     </div>
                 )))}
+                {/* {information && (information.map((supplier: SupplierType) => (
+
+                    <div key={supplier.id} className="grid grid-cols-6 ml-5 border-2 w-full text-amber-50">
+                        <p className="w-[230px] h-10">{supplier.legal_name}</p>
+                        <p className="w-[230px] h-10">{supplier.trade_name}</p>
+                        <p className="w-[230px] h-10">{supplier.cnpj}</p>
+                        <p className="w-[230px] h-10">{supplier.value}</p>
+                        <p className="w-[230px] h-10">{supplier.registration_date}</p>
+                        <div className="flex gap-2 w-fit"><p>editar</p> <p>excluir</p></div>
+                    </div>
+                )))} */}
             </div>
         </>
     )
--- src/pages/supplier/Suppliers.tsx
+++ src/pages/supplier/Suppliers.tsx
@@ -7,19 +7,33 @@ import { SupplierCard } from "./SupplierCard";
 
 
 export const Suppliers = () => {
-    const {data, isLoading, isError,error} = useSupplier({"fields": "phone,legal_name,trade_name,id,registration_date,cnpj"});
+    const { data, isLoading, isError, error } = useSupplier({ "fields": "phone,legal_name,trade_name,id,registration_date,cnpj" });
 
     const information = data?.data;
     console.log(data);
-    if(isError) console.log(error.message)
+    if (isError) console.log(error.message)
     return (
         <div>
-            <h1 className="font-bold text-4xl">Fornecedores</h1>
+            <div className="flex justify-between mt-16">
+                <h1 className="ml-9 font-bold text-[#E0E2EB] text-[25px]">Fornecedores</h1>
+                <div className="text-amber-50">
+                    {/* depois trocar pro componente de pesquisa */}
+                    <p>Pesquisar</p>
+                    <div>
+                        <div>
+                            ordernar por:
+                            imgpbaixo
+                        </div>
+                        imgpcima
+                    </div>
+                    <button> + adicionar novo</button>
+                </div>
+            </div>
             {isLoading && <p>Carregando...</p>}
-            <SupplierCard information={information as SupplierType[]}/>
-            
+            <SupplierCard information={information as SupplierType[]} />
 
-            
+
+
         </div>
     )
 }
